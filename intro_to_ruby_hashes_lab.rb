7612ddd3063f7fa4fd27f08dfc9622d35657c9c6
Jordan Dains <dains.jordan2@gmail.com>
2020-09-14 15:37:05 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -4,8 +4,7 @@ end
 
 def monopoly_with_second_tier
   monopoly_with_second_tier = {
-    :railroads => {:pieces => 4}
-    
+    :railroads => {:pieces => 4, :rent_in_dollars => {}, :names => {}}
   }
   
 
