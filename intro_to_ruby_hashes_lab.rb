1bfac1529f8e6c18ad89ed03c253d05fcf40de4e
Jordan Dains <dains.jordan2@gmail.com>
2020-09-14 15:38:55 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -10,7 +10,7 @@ end
 
 def monopoly_with_third_tier
   monopoly_with_third_tier_tier = {
-    :railroads => {:pieces => 4, :rent_in_dollars => {:one_piece_owned => 25, :two_pieces_owned}, :names => {}}
+    :railroads => {:pieces => 4, :rent_in_dollars => {:one_piece_owned => 25, :two_pieces_owned => 50, :three_pieces_owned => 100, }, :names => {}}
   }
 end
 
